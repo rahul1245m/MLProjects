bf130547e4fab3a8b3d253263ccab73819e59f92
rahul1245m <rahul12mgowda@gmail.com>
2025-04-20 13:16:39 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,15 +1,19 @@
-import os
 import sys
+import os
 from src.exception import CustomerException
 from src.logger import logging
 import pandas as pd 
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
-
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainingConfig
+
 import warnings
+warnings.filterwarnings("ignore")
 
 @dataclass 
 class DataIngestionConfig:
@@ -55,4 +59,10 @@ if __name__ == "__main__":
 
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
